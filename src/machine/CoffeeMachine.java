ca01ef088d1db4016e52c27802f60332f758c043
Andrii Herman <andrii.herman.w@gmail.com>
2021-12-12 23:27:31 +0200

    Create CoffeeMachine

---
+++ src/machine/CoffeeMachine.java
@@ -0,0 +1,140 @@
+package machine;
+
+public class CoffeeMachine {
+
+    private CoffeeMachineState machineState;
+    private int money, water, milk, coffeeBeans, cups;
+
+    public CoffeeMachine() {
+        machineState = CoffeeMachineState.TURNED_OFF;
+        money = 550;
+        water = 400;
+        milk = 540;
+        coffeeBeans = 120;
+        cups = 9;
+    }
+
+    public void pressPowerButton() {
+        machineState = machineState == CoffeeMachineState.TURNED_OFF ?
+                CoffeeMachineState.CHOOSING_AN_ACTION :
+                CoffeeMachineState.TURNED_OFF;
+
+        showMessage(machineState.getMessage());
+    }
+
+    public void interact(String userOption) {
+        try {
+            if (machineState == CoffeeMachineState.CHOOSING_AN_ACTION)
+                chooseAction(userOption);
+            else if (machineState == CoffeeMachineState.CHOOSING_A_COFFEE)
+                chooseCoffee(userOption);
+            else if (machineState == CoffeeMachineState.FILL)
+                fill(userOption);
+
+        } catch (IllegalArgumentException exception) {
+            showMessage(exception.getMessage());
+        }
+
+        showMessage(machineState.getMessage());
+    }
+
+    private void chooseAction(String action) throws IllegalArgumentException {
+        switch (action) {
+            case "buy" -> machineState = CoffeeMachineState.CHOOSING_A_COFFEE;
+            case "fill" -> machineState = CoffeeMachineState.FILL;
+            case "remaining" -> displayRemaining();
+            case "take" -> take();
+            case "exit" -> {
+                showMessage("Bye!");
+                machineState = CoffeeMachineState.TURNED_OFF;
+            }
+            default -> throw new IllegalArgumentException("Incorrect action. Please try again.");
+        }
+    }
+
+    private void chooseCoffee(String option) throws IllegalArgumentException {
+        Coffee coffee;
+
+        switch (option) {
+            case "1":
+                coffee = Coffee.ESPRESSO;
+                break;
+            case "2":
+                coffee = Coffee.LATTE;
+                break;
+            case "3":
+                coffee = Coffee.CAPPUCCINO;
+                break;
+            case "back":
+                return;
+            default:
+                throw new IllegalArgumentException("Incorrect option. Choose coffee number or return back.");
+        }
+
+        checkIngredientsFor(coffee);
+        makeCoffee(coffee);
+
+        machineState = CoffeeMachineState.CHOOSING_AN_ACTION;
+    }
+
+    private void checkIngredientsFor(Coffee coffee) throws IllegalArgumentException {
+        String error = "";
+
+        if (cups < 1) error = "cups";
+        if (coffee.getCoffeeBeans() > coffeeBeans) error = "coffee beans";
+        if (coffee.getMilk() > milk) error = "milk";
+        if (coffee.getWater() > water) error = "water";
+
+        if (!error.isEmpty())
+            throw new IllegalArgumentException(String.format("Sorry, not enough %s!", error));
+    }
+
+    private void makeCoffee(Coffee coffee) {
+        showMessage("I have enough resources, making you a coffee!");
+
+        money += coffee.getPrice();
+        water -= coffee.getWater();
+        milk -= coffee.getMilk();
+        coffeeBeans -= coffee.getCoffeeBeans();
+        cups--;
+    }
+
+    private void fill(String ingredients) throws IllegalArgumentException {
+        String[] resources = ingredients.split(" ");
+        try {
+            water += Integer.parseInt(resources[0]);
+            milk += Integer.parseInt(resources[1]);
+            coffeeBeans += Integer.parseInt(resources[2]);
+            cups += Integer.parseInt(resources[3]);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Incorrect input. Please enter correct amount of ingredients.");
+        }
+
+        machineState = CoffeeMachineState.CHOOSING_AN_ACTION;
+    }
+
+    private void displayRemaining() {
+        showMessage(String.format("""
+                        The coffee machine has:
+                        %d ml of water
+                        %d ml of milk
+                        %d g of coffee beans
+                        %d disposable cups
+                        $%d of money""",
+                water, milk, coffeeBeans, cups, money)
+        );
+    }
+
+    private void take() {
+        showMessage(String.format("I gave you $%d", money));
+        money = 0;
+    }
+
+    private void showMessage(String message) {
+        System.out.printf("\n%s\n", message);
+    }
+
+    public CoffeeMachineState getMachineState() {
+        return machineState;
+    }
+}
--- src/machine/Main.java
+++ src/machine/Main.java
@@ -1,8 +1,17 @@
 package machine;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        CoffeeMachine coffeeMachine = new CoffeeMachine();
+
+        coffeeMachine.pressPowerButton();
+
+        do {
+            coffeeMachine.interact(scanner.nextLine());
+        } while (coffeeMachine.getMachineState() != CoffeeMachineState.TURNED_OFF);
     }
 }
